eee88be700ad9b6dcc512fb9fc90abff03d4bb9e
AmitKarnam <amitkarnam01@gmail.com>
2025-04-23 18:50:22 +0000

    refactor: Improve state token management in Google OAuth flow

--- internal/controller/googleAuth.go
+++ internal/controller/googleAuth.go
@@ -4,13 +4,18 @@ import (
 	"context"
 	"crypto/rand"
 	"encoding/base64"
+	"fmt"
 	"net/http"
+	"os"
+	"sync"
 
 	"github.com/AmitKarnam/WorkoutTracker/internal/services"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
-var stateToken string // Temporary storage for the state token (TODO : Need to use session based token management)
+var sessionState = make(map[string]string)
+var mutex = &sync.Mutex{}
 
 type AuthController struct {
 	authService services.AuthService
@@ -29,7 +34,16 @@ func generateStateToken() string {
 
 func (ac *AuthController) GoogleLogin(c *gin.Context) {
 	// Generate and store the state token
-	stateToken = generateStateToken()
+	stateToken := generateStateToken()
+	session_id := uuid.New().String()
+
+	// Protect the sessionState map with a mutex
+	mutex.Lock()
+	sessionState[session_id] = stateToken
+	mutex.Unlock()
+
+	// TODO : the values for setting a cookie should be fetched from constant
+	c.SetCookie("google_auth_session", session_id, 3600, "/", os.Getenv("DOMAIN"), false, true)
 
 	// Pass the state token to the OAuth URL
 	url := ac.authService.GetGoogleOAuthURL(stateToken)
@@ -41,8 +55,29 @@ func (ac *AuthController) GoogleCallback(c *gin.Context) {
 	state := c.Query("state")
 	code := c.Query("code")
 
+	// Get session_id from the request object, that is passed as http-only cookie
+	session_id, err := c.Cookie("google_auth_session")
+	fmt.Println("session id: ", session_id)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Cookie not found"})
+		return
+	}
+
+	// Protect the sessionState map with a mutex
+	mutex.Lock()
+	session_state, exists := sessionState[session_id]
+	if exists {
+		delete(sessionState, session_id)
+	}
+	mutex.Unlock()
+
+	if !exists {
+		c.JSON(http.StatusNotFound, gin.H{"error": "session id does not exsist"})
+		return
+	}
+
 	// Validate the state token
-	if state != stateToken {
+	if state != session_state {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid state token"})
 		return
 	}
